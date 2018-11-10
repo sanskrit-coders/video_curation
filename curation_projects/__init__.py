a124f603af42ae3f4133dd8b0f5e8a1c3ab47a00
vvasuki <vishvas.vasuki@gmail.com>
2018-11-10 14:40:03 -0800

    Content update.

--- curation_projects/__init__.py
+++ curation_projects/__init__.py
@@ -1,3 +1,3 @@
 """
-Particular audio file curation projects which illustrate the use of :py:module:audio_curation .
+Particular video file curation projects which illustrate the use of :py:module:video_curation .
 """
